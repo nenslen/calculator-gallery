3b5407881e7b0f76bf93a6c022b77926a3bf6114
nenslen <nicolas.enslen@gmail.com>
2018-10-13 18:21:03 -0700

    Fixed issue where decimal places setting didn't display properly

--- js/app-controller.js
+++ js/app-controller.js
@@ -31,16 +31,16 @@ angular.module('calculatorApp', []).controller('app-controller', ['$scope', func
 
 	$scope.decimalPlacesOptions = [
 		'auto',
-		'0',
-		'1',
-		'2',
-		'3',
-		'4',
-		'5',
-		'6',
-		'7',
-		'8',
-		'9'
+		0,
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9
 	];
 
 	/**
