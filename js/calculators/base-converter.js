54b53aa5aacde9b26eca4d5f38d81acd6950f34d
Nic Enslen <nicolas.enslen@gmail.com>
2018-11-07 17:00:40 -0800

    Base converter calculator works

--- js/calculators/base-converter.js
+++ js/calculators/base-converter.js
@@ -13,8 +13,22 @@ angular.module('calculatorApp').controller('base-converter', ['$scope', function
 
 	let outputs = {};
 	
-	let calculate = function(type) {
-		console.log(inputs[type]);
+	let baseNumbers = {
+		bin: 2,
+		dec: 10,
+		hex: 16,
+	};
+
+	let calculate = function(inputBase) {
+		let inputValue = inputs[inputBase].value;
+		let inputBaseNumber = baseNumbers[inputBase];
+		
+		for (let base in this.inputs) {
+			if (inputBase == base) { continue; }
+
+			let baseNumber = baseNumbers[base];
+			this.inputs[base].value = convertBase(inputValue, inputBaseNumber, baseNumber);
+		}
 
 		this.errors = [];
 		this.truncateOutputs();
@@ -23,3 +37,22 @@ angular.module('calculatorApp').controller('base-converter', ['$scope', function
 	$scope.calc = new Calculator(title, name, description, inputs, outputs, calculate);
 	$scope.initializeCalculator($scope.calc);
 }]);
+
+function convertBase(value, from_base, to_base) {
+	var range = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/'.split('');
+	var from_range = range.slice(0, from_base);
+	var to_range = range.slice(0, to_base);
+
+	var dec_value = value.split('').reverse().reduce(function (carry, digit, index) {
+	if (from_range.indexOf(digit) === -1) throw new Error('Invalid digit `'+digit+'` for base '+from_base+'.');
+		return carry += from_range.indexOf(digit) * (Math.pow(from_base, index));
+	}, 0);
+
+	var new_value = '';
+	while (dec_value > 0) {
+		new_value = to_range[dec_value % to_base] + new_value;
+		dec_value = (dec_value - (dec_value % to_base)) / to_base;
+	}
+	return new_value || '0';
+}
+
