d665e06fd981cbc8cce8ca6ee99a4f0f8dfd8e3d
Nic Enslen <nicolas.enslen@gmail.com>
2018-11-21 20:35:24 -0800

    Finished average calc

--- js/calculators/average.js
+++ js/calculators/average.js
@@ -10,7 +10,7 @@ angular.module('calculatorApp').controller('average', ['$scope', function($scope
 	let outputs = {
 		mean: new Output('Mean', 0, 'number'),
 		median: new Output('Median', 0, 'number'),
-		mode: new Output('Mode', 0, 'number'),
+		mode: new Output('Mode', 0, 'text'),
 		range: new Output('Range', 0, 'number'),
 	};
 	
@@ -24,27 +24,12 @@ angular.module('calculatorApp').controller('average', ['$scope', function($scope
 			.sort(function(a, b) {
 				return a - b;
 			});
-		
-		let count = numbers.length;
-		let sum = 0;
-		let mean = 0;
-		if (count > 0) {
-			sum = numbers.reduce(function(total, number) {
-			    return total + number;
-			});
-			mean = sum / count;
-		}
-		
-		let middleIndex = Math.floor(numbers.length / 2);
-		let median = 0;
-		if (count % 2 === 1) {
-			median = numbers[middleIndex];
-		} else {
-			median = (numbers[middleIndex - 1] + numbers[middleIndex]) / 2.0;
-		}
 
-		this.outputs.mean.value = mean;
-		this.outputs.median.value = median;
+		this.outputs.mean.value = mean(numbers);
+		this.outputs.median.value = median(numbers);
+		this.outputs.mode.value = mode(numbers);
+		this.outputs.range.value = range(numbers);
+
 		this.errors = [];
 		this.truncateOutputs();
 	};
@@ -52,3 +37,77 @@ angular.module('calculatorApp').controller('average', ['$scope', function($scope
 	$scope.calc = new Calculator(title, name, description, inputs, outputs, calculate);
 	$scope.initializeCalculator($scope.calc);
 }]);
+
+function mean(numbers) {
+	let count = numbers.length;
+	let sum = 0;
+	let mean = 0;
+
+	if (count > 0) {
+		sum = numbers.reduce(function(total, number) {
+		    return total + number;
+		});
+		mean = sum / count;
+	}
+
+	return mean;
+}
+
+function median(numbers) {
+	let count = numbers.length;
+	let middleIndex = Math.floor(numbers.length / 2);
+	let median = 0;
+	
+	if (count > 0) {
+		if (count % 2 === 1) {
+			median = numbers[middleIndex];
+		} else {
+			median = (numbers[middleIndex - 1] + numbers[middleIndex]) / 2.0;
+		}
+	}
+
+	return median;
+}
+
+function mode(numbers) {
+	let counts = [];
+
+	// Count how many times each number appears
+	for (let i = 0; i < numbers.length; i++) {
+		let number = numbers[i];
+
+		if (counts[number] !== undefined) {
+			counts[number]++;
+		} else {
+			counts[number] = 1;
+		}
+	}
+
+	// Find the number with the highest count
+	let highestCount = 0;
+	let mode = 'n/a';
+
+	for (let number in counts) {
+		if (counts[number] > highestCount) {
+			highestCount = counts[number];
+			mode = number;
+		}
+	}
+
+	if (highestCount <= 1) {
+		return 'n/a';
+	}
+	
+	return mode + ' (' + highestCount + ' times)';
+}
+
+function range(numbers) {
+	let smallest = numbers[0];
+	let largest = numbers[numbers.length - 1];
+
+	if (smallest === undefined || largest === undefined) {
+		return 'n/a';
+	}
+
+	return largest - smallest;
+}
